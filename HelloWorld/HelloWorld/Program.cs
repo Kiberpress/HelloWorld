3c3b1844ef86483d2033c0592e72cf2b8cf8a571
Kiberpress <kiberpress@gmail.com>
2018-06-06 14:49:34 -0400

    Update output string

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello world!");
+            Console.WriteLine("Let's see how it will look in GitHub with new changes!");
             Console.ReadLine();
         }
     }
