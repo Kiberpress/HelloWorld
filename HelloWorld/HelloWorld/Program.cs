8bfdec42547139e56d0c5aba5055c10da0fd8a3c
Kiberpress <40003683+Kiberpress@users.noreply.github.com>
2018-06-06 15:36:20 -0400

    Commit changes to TestBranch

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Let's see how it will look in GitHub with new changes!");
+            Console.WriteLine("Let's change something in new branch!");
             Console.ReadLine();
         }
     }
